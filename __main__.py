0edb04c89fd25a31ebb11bdc7f8bd35b02850868
wfan19 <1472451850@qq.com>
2018-05-23 08:17:06 -0400

    missed the new things in main

--- __main__.py
+++ __main__.py
@@ -13,14 +13,29 @@ def getPrefix(bot, message):
 
 #list of cogs files
 
-bot = commands.Bot(commands_prefix = getPrefix, description = 'Bill\'s gambling bot') #bot creation
+bot = commands.Bot(command_prefix = getPrefix, description = 'Bill\'s gambling bot') #bot creation
 
+"""
+#Derek's fancy cog finder
 for ext in os.listdir('Bill Bot/cogs'):
     if not ext.startswith(('_', '.')):
         bot.load_extension('Bill Bot.cogs.' + ext[:-3])  # Remove '.py'
+"""
 
+initial_extensions = ['cogs.general',
+                                 'cogs.seven',
+                                 'cogs.blackJack']
 
-@bot.event()
+#Eviee way of loading cogs (More manual)
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+@bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
